42b4da19b61ebe8039685786f59b15b73b12fa9d
AbhiSharma096 <Abhisheksharma10a@gmail.com>
2024-07-17 23:46:00 +0530

    Bottom navigation Bar completed

--- app/src/main/java/com/example/transformer/MainActivity.kt
+++ app/src/main/java/com/example/transformer/MainActivity.kt
@@ -1,29 +1,125 @@
 package com.example.transformer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.transformer.ui.theme.TransformerTheme
 
+data class BottomNavigationItems(
+    val tittle: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val BadgeCount: Int? = null
+)
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TransformerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+                val items = listOf(
+                    BottomNavigationItems(
+                        "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false
+                    ),
+                    BottomNavigationItems(
+                        "Home",
+                        selectedIcon = Icons.Filled.Add,
+                        unselectedIcon = Icons.Outlined.Add,
+                        hasNews = false
+                    ),
+                    BottomNavigationItems(
+                        "Home",
+                        selectedIcon = Icons.Filled.Build,
+                        unselectedIcon = Icons.Outlined.Build,
+                        hasNews = true
                     )
+                )
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
+                Surface(
+                    modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(bottomBar = {
+                        NavigationBar {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    selected = selectedItemIndex == index,
+                                    onClick = {
+                                        selectedItemIndex = index
+                                            // navController.navigate(item.tittle)
+                                        },
+                                    label = {
+                                    Text(text = item.tittle.toString()) },
+                                    icon = {
+                                    BadgedBox(badge = {
+                                        if (item.BadgeCount != null) {
+                                            Badge {
+                                                Text(text = item.BadgeCount.toString())
+                                            }
+                                        } else if (item.hasNews) {
+                                            Badge()
+                                        }
+                                    }) {
+                                        Icon(
+                                            imageVector = if (index == selectedItemIndex) {
+                                                item.selectedIcon
+                                            } else {
+                                                item.unselectedIcon
+                                            }, contentDescription = item.tittle
+                                        )
+
+                                    }
+                                })
+                            }
+                        }
+                    })
+                    {
+
+                    }
                 }
             }
         }
